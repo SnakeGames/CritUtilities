6896a45d0432562bdd1dfc97fe88a33434c29a01
SnakeGames <61299588+SnakeGames@users.noreply.github.com>
2020-06-07 09:42:02 -0400

    a

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ fs.readdir("./commands/", (err, files) => {
   let jsfile = files.filter(f => f.split(".").pop() === "js")
   if(jsfile.length <= 0) {
     return console.log("[LOGS] Couldn't find commands!");
-  } 
+  }
 
   jsfile.forEach((f, i) =>{
     let pull = require(`./commands/${f}`);
@@ -46,4 +46,4 @@ bot.on("message", async message => {
   if(commandfile) commandfile.run(bot,message,args)
 
 });
-bot.login(botconfig.token);
+bot.login(process.env.token);
